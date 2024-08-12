e2e14892131b3428db99d86dd23b0a7caebbd97f
Gopal Dixit <98686075+GopalDixit@users.noreply.github.com>
2024-08-12 16:12:12 +0530

    Update mongo.js

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/E-commerce')
+mongoose.connect('mongodb+srv://gopaldixit9450:N4tEEMO3UwssJiYP@admincluster.rgu9w.mongodb.net/E-com?retryWrites=true&w=majority&appName=admincluster')
 .then(()=>{
     console.log("MongpDB Connected")
 })
